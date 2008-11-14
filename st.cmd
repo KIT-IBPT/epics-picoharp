8e25703d7fb082ae56d31f247fad0aadfd757200
jr76 <jr76@e099a375-04f9-0310-9d5f-a741eaff62e1>
2008-11-14 15:33:15 +0000

    picoharp: move db
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/picoharp@25469 e099a375-04f9-0310-9d5f-a741eaff62e1

--- st.cmd
+++ st.cmd
@@ -2,7 +2,7 @@
 
 dbLoadDatabase("dbd/picoharp.dbd")
 picoharp_registerRecordDeviceDriver(pdbbase)
-dbLoadRecords("test.db")
+dbLoadRecords("db/picoharp.db", "DEVICE=TS-DI-PICO-02")
 
 # Port, Event, Offset, CFDLevel0, CFDLevel1, CFDZeroX0, CFDZeroX1, SyncDiv, Range
 initPicoAsyn("PICO1", 101, 0, 300, 20, 10, 11, 1, 3)
