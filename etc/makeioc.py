c64818ea35d562f2ab3d3a86c402fbc78bcd241e
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2015-04-10 13:40:13 +0000

    Picoharp: Update example IOC
    Also add PV logging to example IOC to help with observing behaviour.
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/picoharp@131071 e099a375-04f9-0310-9d5f-a741eaff62e1

--- etc/makeioc.py
+++ etc/makeioc.py
@@ -7,19 +7,21 @@ import iocbuilder
 iocbuilder.ConfigureIOC(architecture = 'linux-x86_64')
 from iocbuilder import ModuleVersion
 
+ModuleVersion('pvlogging', '1-1-1')
 ModuleVersion('asyn',      '4-21')
 ModuleVersion('picoharp',  home = '..', use_name = False)
 
 from iocbuilder.modules import picoharp
+from iocbuilder.modules import pvlogging
 
 
-ioc_name = 'BL01B-DI-IOC-03'
+ioc_name = 'BL01B-DI-IOC-12'
 
 iocbuilder.EpicsEnvSet('EPICS_CA_MAX_ARRAY_BYTES', 2000000)
 
 # Picoharp fill pattern monitor
-picoharp.PicoharpInstance('BL01B-DI-PICO-02', '1013107')
-# picoharp.PicoharpInstance('BL01B-DI-PICO-01', '1001267')
+charge = 'SR23C-DI-DCCT-01:CHARGE'
+picoharp.PicoharpInstance('BL01B-DI-PICO-01', '1013107', charge)
 
-
+pvlogging.PvLogging()
 iocbuilder.WriteNamedIoc('ioc', ioc_name)
