KIT-IBPT/epics-picoharp

commit c4c2217c190b3f66621245c192fa74ada5b667c8

Picoharp: Dynamically create arrays
Use the passed in bucket count etc to compute waveforms dynamically.  This
should now allow the picoharp software to work on different ring sizes.

git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/picoharp@131074 e099a375-04f9-0310-9d5f-a741eaff62e1

picoharpApp/src/picopeaks.c
@@ -14,12 +14,8 @@
 
 #define LOG_PLOT_OFFSET     1e-20
 
-/* Variable recording the start of each bucket in the raw sample profile. */
-static int bucket_start[BUCKETS];
 
-
-/* TODO return from function on error */
-
+/* Returns from function on error */
 #define PICO_CHECK(call) \
     { \
         int __status = call; \
@@ -109,18 +105,18 @@ static double sum_peaks(
     int end_offset = start_offset + (int) self->sample_width;
     if (start_offset < 0)
         start_offset = 0;
-    if (end_offset > SAMPLES_PER_PROFILE)
-        end_offset = SAMPLES_PER_PROFILE;
+    if (end_offset > self->samples_per_bucket)
+        end_offset = self->samples_per_bucket;
 
     double total = 0;
-    for (int k = 0; k < BUCKETS; ++k)
+    for (int k = 0; k < self->bucket_count; ++k)
     {
-        int start = bucket_start[k] + start_offset;
-        int end = bucket_start[k] + end_offset;
+        int start = self->bucket_start[k] + start_offset;
+        int end = self->bucket_start[k] + end_offset;
         double sum = 0;
         for (int j = start; j < end; j ++)
             sum += samples[j];
-        bins[(k + BUCKETS - shift) % BUCKETS] = sum;
+        bins[(k + self->bucket_count - shift) % self->bucket_count] = sum;
         total += sum;
     }
     return total;
@@ -132,13 +128,14 @@ static double compute_pileup(
 {
     int length = (int) self->deadtime;
     int shift = (int) self->shift;
-    bucket = (bucket + shift) % BUCKETS;
-    int start_bucket = (bucket + BUCKETS - length) % BUCKETS;
-    int start = bucket_start[start_bucket];
-    int end = bucket_start[bucket];
+    bucket = (bucket + shift) % self->bucket_count;
+    int start_bucket =
+        (bucket + self->bucket_count - length) % self->bucket_count;
+    int start = self->bucket_start[start_bucket];
+    int end = self->bucket_start[bucket];
 
     double pileup = 0;
-    for (int i = start; i != end; i = (i + 1) % VALID_SAMPLES)
+    for (int i = start; i != end; i = (i + 1) % self->valid_samples)
         pileup += samples[i];
     return pileup;
 }
@@ -154,7 +151,7 @@ static void correct_peaks(
 {
     *max_fixup = 0;
     double total_counts = 0;
-    for (int i = 0; i < BUCKETS; i ++)
+    for (int i = 0; i < self->bucket_count; i ++)
     {
         /* Compute pileup factor by counting number of observed events preceding
          * this one.  We convert counts into counts per turn. */
@@ -172,37 +169,33 @@ static void correct_peaks(
 
     /* Scale buckets by pileup correction and current scaling factor. */
     double charge = 1e6 * self->current / self->turns_per_sec;
-    for (int i = 0; i < BUCKETS; i ++)
+    for (int i = 0; i < self->bucket_count; i ++)
         if (total_counts > 0)
-        {
             buckets[i] *= charge / total_counts;
-            if (buckets[i] < LOG_PLOT_OFFSET)
-                /* Fudge for EDM display. */
-                buckets[i] = LOG_PLOT_OFFSET;
-        }
         else
             buckets[i] = 0;
 }
 
 
 /* Compute profile by folding all buckets together. */
-static void compute_profile(const double samples[], double profile[])
+static void compute_profile(
+    struct pico_data *self, const double samples[], double profile[])
 {
-    memset(profile, 0, SAMPLES_PER_PROFILE * sizeof(double));
-    for (int n = 0; n < BUCKETS; n++)
+    memset(profile, 0, (size_t) self->samples_per_bucket * sizeof(double));
+    for (int n = 0; n < self->bucket_count; n++)
     {
-        int ix = bucket_start[n];
-        for (int s = 0; s < SAMPLES_PER_PROFILE; s++)
+        int ix = self->bucket_start[n];
+        for (int s = 0; s < self->samples_per_bucket; s++)
             profile[s] += samples[ix + s];
     }
 }
 
 /* Discover the peak of the profile. */
-static int compute_peak(const double profile[])
+static int compute_peak(struct pico_data *self, const double profile[])
 {
     double max = 0;
     int peak = 0;
-    for (int i = 0; i < SAMPLES_PER_PROFILE; i ++)
+    for (int i = 0; i < self->samples_per_bucket; i ++)
     {
         if (profile[i] > max)
         {
@@ -244,6 +237,18 @@ static void compute_flux(
 }
 
 
+/* This code is needed to work around a misfeature of EDM: when displaying a
+ * waveform with a logarithmic axis EDM handles zero (and negative) values
+ * quite badly.  We fudge this by hacking the vector to force a smallest
+ * display value. */
+static void fixup_display_vector(double vector[], int length)
+{
+    for (int n = 0; n < length; n ++)
+        if (vector[n] < LOG_PLOT_OFFSET)
+            vector[n] = LOG_PLOT_OFFSET;
+}
+
+
 /* The monstrous list of parameters in this function should really be gathered
  * into one or more structures, but the current architecture makes this
  * exceptionally messy to do. */
@@ -255,8 +260,8 @@ static void process_pico_peaks(
 {
     compute_flux(self, turns, total_count, flux, nflux);
 
-    compute_profile(samples, profile);
-    *peak = compute_peak(profile);
+    compute_profile(self, samples, profile);
+    *peak = compute_peak(self, profile);
 
     sum_peaks(self, (int) *peak, (int) self->shift, samples, raw_buckets);
 
@@ -265,13 +270,13 @@ static void process_pico_peaks(
         turns, samples, raw_buckets, fixup, max_fixup, buckets);
 
     *socs = 0;
-    for (int k = 0; k < BUCKETS; ++k)
+    for (int k = 0; k < self->bucket_count; ++k)
         *socs += buckets[k] * buckets[k];
 
-    /* Final fudge on raw samples for EDM display. */
-    for (int n = 0; n < HISTCHAN; n ++)
-        if (samples[n] < LOG_PLOT_OFFSET)
-            samples[n] = LOG_PLOT_OFFSET;
+    /* Fixup values for display. */
+    fixup_display_vector(samples, HISTCHAN);
+    fixup_display_vector(buckets, self->bucket_count);
+    fixup_display_vector(profile, self->samples_per_bucket);
 }
 
 #define PROCESS_PICO_PEAKS(self, period) \
@@ -357,7 +362,7 @@ static void reset_accum(struct pico_data *self)
 
 void pico_process_5s(struct pico_data *self)
 {
-    memcpy(self->samples_5, self->buffer5, sizeof(self->samples_5));
+    memcpy(self->samples_5, self->buffer5, sizeof(self->buffer5));
     self->turns_5 = self->turns_buffer5;
     self->total_count_5 = self->count_buffer5;
 
@@ -476,12 +481,30 @@ static bool pico_open(struct pico_data *self)
     return pico_set_config(self);
 }
 
+
+#define CREATE_ARRAY_RANGE(self, name, length) \
+    self->name##_fast = calloc((size_t) length, sizeof(double)); \
+    self->name##_5    = calloc((size_t) length, sizeof(double)); \
+    self->name##_60   = calloc((size_t) length, sizeof(double)); \
+    self->name##_180  = calloc((size_t) length, sizeof(double)); \
+    self->name##_all  = calloc((size_t) length, sizeof(double));
+
+
 bool pico_init(struct pico_data *self, const char *serial)
 {
+    /* Initialise dynamically created arrays. */
+    CREATE_ARRAY_RANGE(self, samples, HISTCHAN);
+    CREATE_ARRAY_RANGE(self, profile, self->samples_per_bucket);
+    CREATE_ARRAY_RANGE(self, buckets, self->bucket_count);
+    CREATE_ARRAY_RANGE(self, raw_buckets, self->bucket_count);
+    CREATE_ARRAY_RANGE(self, fixup, self->bucket_count);
+
     /* Initialise the bucket boundaries.  Doesn't matter if we do this more than
      * once, the result is the same. */
-    for (int i = 0; i < BUCKETS; i ++)
-        bucket_start[i] = (int) round((float) i * VALID_SAMPLES / BUCKETS);
+    self->bucket_start = calloc((size_t) self->bucket_count, sizeof(int));
+    for (int i = 0; i < self->bucket_count; i ++)
+        self->bucket_start[i] = (int) lround(
+            (double) i * self->valid_samples / self->bucket_count);
 
     /* initialize non zero defaults */
     self->sample_width = 1;

picoharpApp/src/asynHelper.h
@@ -22,11 +22,10 @@
     { \
         .offset = offsetof(STRUCT, MEMBER), \
         .name = #MEMBER, \
-        .elements = (sizeof((STRUCT *)NULL)->MEMBER) / sizeof(TYPE), \
         ALARMED \
     }
 
-#define EXPORT_ARRAY_END {0, 0, 0, 0}
+#define EXPORT_ARRAY_END {0}
 
 #define MEMBER_LOOKUP(s, info) (((char *) (s)) + (info)->offset)
 
@@ -34,7 +33,6 @@ struct struct_info
 {
     int offset;
     const char * name;
-    size_t elements;
     bool alarmed;       // If set an error is raised on alarm
     bool notify;        // If set the updated flag is set if field written
 };

picoharpApp/src/picoAsyn.c
@@ -116,51 +116,32 @@ struct pico_pvt
     struct pico_data data;
 };
 
-/* float 64 array */
 
-static asynStatus pico_write(
-    void *drvPvt, asynUser *pasynUser, epicsFloat64 *value, size_t elements)
+
+static const struct struct_info *lookup_info(void *drvPvt, asynUser *pasynUser)
 {
     struct pico_pvt *pico = drvPvt;
-    struct pico_data *data = &pico->data;
-
     int field = pasynUser->reason;
     if (field < 0)
-        return asynError;
-    const struct struct_info *info = &pico->info[field];
-
-    if (elements > info->elements)
-        return asynError;
-
-    epicsMutexMustLock(pico->lock);
-
-    memcpy(MEMBER_LOOKUP(data, info), value, elements * sizeof(epicsFloat64));
-    if (info->notify)
-        data->parameter_updated = true;
-
-    epicsMutexUnlock(pico->lock);
-
-    return asynSuccess;
+        return NULL;
+    else
+        return &pico->info[field];
 }
 
-static asynStatus pico_read(
+
+static asynStatus pico_read_array(
     void *drvPvt, asynUser *pasynUser, epicsFloat64 *value,
     size_t elements, size_t *nIn)
 {
+    const struct struct_info *info = lookup_info(drvPvt, pasynUser);
+    if (info == NULL)
+        return asynError;
+
     struct pico_pvt *pico = drvPvt;
-    struct pico_data *data = &pico->data;
-
-    int field = pasynUser->reason;
-    if (field < 0)
-        return asynError;
-    const struct struct_info *info = &pico->info[field];
-
-    if (elements > info->elements)
-        return asynError;
-
     epicsMutexMustLock(pico->lock);
 
-    memcpy(value, MEMBER_LOOKUP(data, info), elements * sizeof(epicsFloat64));
+    double *array = *(double **) MEMBER_LOOKUP(&pico->data, info);
+    memcpy(value, array, elements * sizeof(epicsFloat64));
     *nIn = elements;
     bool alarm = pico->alarm && info->alarmed;
 
@@ -169,46 +150,66 @@ static asynStatus pico_read(
     return alarm ? asynError : asynSuccess;
 }
 
-static asynStatus pico_write_adapter(
-    void *drvPvt, asynUser *pasynUser, epicsFloat64 value)
-{
-    return pico_write(drvPvt, pasynUser, &value, 1);
-}
-
-static asynStatus pico_read_adapter(
+static asynStatus pico_read_value(
     void *drvPvt, asynUser *pasynUser, epicsFloat64 *value)
 {
-    size_t nIn;
-    return pico_read(drvPvt, pasynUser, value, 1, &nIn);
+    const struct struct_info *info = lookup_info(drvPvt, pasynUser);
+    if (info == NULL)
+        return asynError;
+
+    struct pico_pvt *pico = drvPvt;
+    epicsMutexMustLock(pico->lock);
+
+    *value = *(double *) MEMBER_LOOKUP(&pico->data, info);
+    bool alarm = pico->alarm && info->alarmed;
+
+    epicsMutexUnlock(pico->lock);
+
+    return alarm ? asynError : asynSuccess;
+}
+
+
+static asynStatus pico_write_value(
+    void *drvPvt, asynUser *pasynUser, epicsFloat64 value)
+{
+    const struct struct_info *info = lookup_info(drvPvt, pasynUser);
+    if (info == NULL)
+        return asynError;
+
+    struct pico_pvt *pico = drvPvt;
+    epicsMutexMustLock(pico->lock);
+
+    *(double *) MEMBER_LOOKUP(&pico->data, info) = value;
+
+    epicsMutexUnlock(pico->lock);
+    return asynSuccess;
 }
 
 static asynStatus oct_read(
     void *drvPvt, asynUser *pasynUser, char *value,
     size_t numchars, size_t *nbytesTransferred, int *eomReason)
 {
-    struct pico_pvt *pico = drvPvt;
-    struct pico_data *data = &pico->data;
-    int field = pasynUser->reason;
-    if (field < 0)
+    const struct struct_info *info = lookup_info(drvPvt, pasynUser);
+    if (info == NULL)
         return asynError;
-    const struct struct_info *info = &pico->info[field];
 
-    epicsMutexLock(pico->lock);
-    snprintf(value, numchars, "%s", MEMBER_LOOKUP(data, info));
+    struct pico_pvt *pico = drvPvt;
+    epicsMutexMustLock(pico->lock);
 
+    snprintf(value, numchars, "%s", MEMBER_LOOKUP(&pico->data, info));
     *nbytesTransferred = numchars;
     *eomReason = 0;
+
     epicsMutexUnlock(pico->lock);
     return asynSuccess;
 }
 
 static asynFloat64Array asynFloat64ArrayImpl = {
-    .write = pico_write,
-    .read = pico_read
+    .read = pico_read_array
 };
 static asynFloat64 asynFloat64Impl = {
-    .write = pico_write_adapter,
-    .read = pico_read_adapter
+    .write = pico_write_value,
+    .read = pico_read_value
 };
 static asynCommon asynCommonImpl = {
     .report = common_report,
@@ -309,7 +310,7 @@ static void picoThreadFunc(void *pvt)
 
 static int initPicoAsyn(
     const char *port, const char *serial, int event_fast, int event_5s,
-    int buckets, int bin_size, int valid_samples,
+    int bucket_count, int bin_size, int valid_samples,
     int samples_per_bucket, double turns_per_sec)
 {
     printf("initPicoAsyn('%s')\n", port);
@@ -334,7 +335,7 @@ static int initPicoAsyn(
 
     /* Control parameters from IOC initialisation. */
     pico->data.range = bin_size;
-    pico->data.buckets = buckets;
+    pico->data.bucket_count = bucket_count;
     pico->data.valid_samples = valid_samples;
     pico->data.samples_per_bucket = samples_per_bucket;
     pico->data.turns_per_sec = turns_per_sec;

picoharpApp/src/picopeaks.h
@@ -6,21 +6,16 @@
 #define BLOCK 0
 
 #define ERRBUF 1024
-#define BUCKETS 936
-#define VALID_SAMPLES 58540
-#define SAMPLES_PER_PROFILE 62  /* floor(VALID_SAMPLES/BUCKETS) */
 
-#define BUFFERS_60 12
-/*(60/5)*/
-#define BUFFERS_180 36
-/*(180/5)*/
+#define BUFFERS_60  12  /*(60/5)*/
+#define BUFFERS_180 36  /*(180/5)*/
 
-#define DECLARE_RANGE(name, suffix...) \
-    double name##_fast suffix; \
-    double name##_5 suffix; \
-    double name##_60 suffix; \
-    double name##_180 suffix; \
-    double name##_all suffix
+#define DECLARE_RANGE(name, prefix...) \
+    double prefix name##_fast; \
+    double prefix name##_5; \
+    double prefix name##_60; \
+    double prefix name##_180; \
+    double prefix name##_all
 
 struct pico_data
 {
@@ -30,7 +25,7 @@ struct pico_data
     /* Configuration settings programmed during initialisation. */
 
     double range;               // Configures picoharp bin size
-    int buckets;                // Number of bunches per machine revolution
+    int bucket_count;           // Number of bunches per machine revolution
     int valid_samples;          // Number of bins per revolution
     int samples_per_bucket;     // Number of bins per bunch
     double turns_per_sec;       // Machine revolutions per second
@@ -40,15 +35,15 @@ struct pico_data
 
     /* These variables belong in one or more structures.  Unfortunately given
      * the history of this project doing this will take too long to do. */
-    DECLARE_RANGE(samples,      [HISTCHAN]);    // Raw samples from picoharp
-    DECLARE_RANGE(raw_buckets,  [BUCKETS]);     // Uncorrected fill patter
-    DECLARE_RANGE(fixup,        [BUCKETS]);     // Correction factor
-    DECLARE_RANGE(max_fixup);                   // Maximum correction factor
-    DECLARE_RANGE(buckets,      [BUCKETS]);     // Corrected fill pattern
+    DECLARE_RANGE(samples, *);  // Raw samples from picoharp
+    DECLARE_RANGE(raw_buckets, *); // Uncorrected fill patter
+    DECLARE_RANGE(fixup, *);    // Correction factor
+    DECLARE_RANGE(max_fixup);   // Maximum correction factor
+    DECLARE_RANGE(buckets, *);  // Corrected fill pattern
     DECLARE_RANGE(socs);        // Sum of Charge Squared
     DECLARE_RANGE(turns);       // Turn count for capture
 
-    DECLARE_RANGE(profile, [SAMPLES_PER_PROFILE]);  // Single bunch profile
+    DECLARE_RANGE(profile, *);  // Single bunch profile
     DECLARE_RANGE(peak);        // Position of profile peak
     DECLARE_RANGE(flux);        // Counts per turn
     DECLARE_RANGE(nflux);       // Counts per turn per nC
@@ -98,6 +93,8 @@ struct pico_data
     /* PicoHarp acquisition results */
     int overflow;
     unsigned int countsbuffer[HISTCHAN];
+
+    int *bucket_start;          // Start of each bucket in raw sample profile
 
 
     int index60;

commit 5d340d6a4da7b78724b1b51ed64d14acc625eac2

Fix long standing bug in long term accumulator functionality
This bug was introduced by commit c4c2217, which introduced dynamically
sized arrays, and used sizeof on a pointer instead of an array.

picoharpApp/src/picopeaks.c
@@ -335,7 +335,7 @@ void pico_process_fast(struct pico_data *self)
 
 static void reset_accum(struct pico_data *self)
 {
-    memset(self->bufferall, 0, sizeof(self->samples_all));
+    memset(self->bufferall, 0, HISTCHAN * sizeof(double));
     self->turns_all = 0;
     self->total_count_all = 0;
     self->reset_accum = 0;